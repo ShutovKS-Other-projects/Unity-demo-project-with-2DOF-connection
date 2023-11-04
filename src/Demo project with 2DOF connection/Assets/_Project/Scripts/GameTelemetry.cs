ea93f28aecb10c59379a937ab478856f5ca621e4
ShutovKS <schutow.kir@yandex.ru>
2023-11-04 11:30:23 +0300

    Added game telemetry

---
+++ src/Demo project with 2DOF connection/Assets/_Project/Scripts/GameTelemetry.cs
@@ -0,0 +1,29 @@
+﻿public class GameTelemetry
+{
+    public double Pitch { get; set; }
+    public double Roll { get; set; }
+    public double Yaw { get; set; }
+    public double Surge { get; set; }
+    public double Sway { get; set; }
+    public double Heave { get; set; }
+    public double Extra1 { get; set; }
+    public double Extra2 { get; set; }
+    public double Extra3 { get; set; }
+    public double Wind { get; set; }
+
+    private double count = 0.0;
+
+    public void Reset()
+    {
+        Pitch = 0.0;
+        Roll = 0.0;
+        Yaw = 0.0;
+        Surge = 0.0;
+        Sway = 0.0;
+        Heave = 0.0;
+        Wind = 0.0;
+        Extra1 = 0.0;
+        Extra2 = 0.0;
+        Extra3 = 0.0;
+    }
+}
