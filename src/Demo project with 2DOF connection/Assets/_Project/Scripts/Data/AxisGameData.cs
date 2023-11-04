10bcace728e5387af1a31975c19eecbf132e26a5
ShutovKS <schutow.kir@yandex.ru>
2023-11-04 15:21:54 +0300

    Added axis game data

---
+++ src/Demo project with 2DOF connection/Assets/_Project/Scripts/Data/AxisGameData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Data
+{
+    [Serializable]
+    public class AxisGameData
+    {
+        public string GameName { get; set; }
+        public byte AxisIndex { get; set; }
+        public int WindProc { get; set; }
+        public int GamePort { get; set; }
+        public int AxisMode { get; set; }
+
+        public AxisGameData(
+            string gameName,
+            byte axisIndex,
+            int gamePort,
+            int windProc,
+            int axisMode)
+        {
+            GameName = gameName;
+            AxisIndex = axisIndex;
+            GamePort = gamePort;
+            AxisMode = axisMode;
+            WindProc = windProc;
+        }
+    }
+}
