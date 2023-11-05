16f0cea3975d4bfddb39409e914c6c5578d497e1
ShutovKS <schutow.kir@yandex.ru>
2023-11-05 08:21:58 +0300

    Added redirect data

--- src/Demo project with 2DOF connection/Assets/_Project/Scripts/Settings.cs
+++ src/Demo project with 2DOF connection/Assets/_Project/Scripts/Settings.cs
@@ -6,6 +6,8 @@ public static class Settings
     public static List<AxisDofData> gameAxes = new();
     public static List<AxisDofData> gameAxes2 = new();
     public static GameSettingsData GameSettingsData;
+    public static AxisGameData AxisGameData;
+    public static RedirectData RedirectData;
 
     public static int shutdownValue = 50;
     public static int windCoefValue = 100;
