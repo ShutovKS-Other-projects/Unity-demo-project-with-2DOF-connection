5d912ada1fe3fdba0e9be5af077d2c95a68f41d5
ShutovKS <schutow.kir@yandex.ru>
2023-11-06 22:36:08 +0300

    Added object telemetry data

--- src/Demo project with 2DOF connection/Assets/_Project/Scripts/CarTelemetryHandler.cs
+++ src/Demo project with 2DOF connection/Assets/_Project/Scripts/CarTelemetryHandler.cs
@@ -1,31 +1,32 @@
+using DOF.Data;
 using UnityEngine;
 
 public class CarTelemetryHandler : MonoBehaviour
 {
     [SerializeField] private Transform vehicleTransform;
-    private GameTelemetry _telemetryData;
+    private ObjectTelemetryData _telemetryDataData;
     
     private void Update()
     {
-        if (_telemetryData == null)
+        if (_telemetryDataData == null)
         {
             return;
         }
         
         var rotation = vehicleTransform.rotation;
-        _telemetryData.Pitch = rotation.eulerAngles.x;
-        _telemetryData.Roll = rotation.eulerAngles.z;
-        _telemetryData.Yaw = rotation.eulerAngles.y;
+        _telemetryDataData.Pitch = rotation.eulerAngles.x;
+        _telemetryDataData.Roll = rotation.eulerAngles.z;
+        _telemetryDataData.Yaw = rotation.eulerAngles.y;
 
         var position = vehicleTransform.position;
-        _telemetryData.Surge = position.z;
-        _telemetryData.Sway = position.x;
-        _telemetryData.Heave = position.y;
+        _telemetryDataData.Surge = position.z;
+        _telemetryDataData.Sway = position.x;
+        _telemetryDataData.Heave = position.y;
 
-        _telemetryData.Extra1 = 0.0;
-        _telemetryData.Extra2 = 0.0;
-        _telemetryData.Extra3 = 0.0;
+        _telemetryDataData.Extra1 = 0.0;
+        _telemetryDataData.Extra2 = 0.0;
+        _telemetryDataData.Extra3 = 0.0;
 
-        _telemetryData.Wind = 0.0;
+        _telemetryDataData.Wind = 0.0;
     }
 }
