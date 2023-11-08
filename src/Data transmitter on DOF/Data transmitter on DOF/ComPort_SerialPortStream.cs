741b2b233bc370098c5ff81c850ddacd480a34a7
ShutovKS <schutow.kir@yandex.ru>
2023-11-08 20:10:10 +0300

    Added data transmitter on dof

---
+++ src/Data transmitter on DOF/Data transmitter on DOF/ComPort_SerialPortStream.cs
@@ -0,0 +1,97 @@
+using RJCP.IO.Ports;
+
+namespace Test_connected_to_COM_Port
+{
+    public static class ComPort_SerialPortStream
+    {
+        private static SerialPortStream serialPort;
+
+        public static bool TryConnect(int comPortNumber = 3, int baudRate = 115200, int dataBits = 8,
+            StopBits stopBits = StopBits.One)
+        {
+            serialPort = new SerialPortStream
+            {
+                BaudRate = baudRate,
+                DataBits = dataBits,
+                Parity = Parity.None,
+                StopBits = stopBits,
+                ReadBufferSize = 4096,
+                WriteBufferSize = 4096,
+                ReadTimeout = 200,
+                PortName = "COM" + comPortNumber
+            };
+
+            Console.WriteLine("SerialPortStream, Connecting to COM port");
+            
+            try
+            {
+                serialPort.Open();
+            }
+            catch
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public static void Disconnect()
+        {
+            Console.WriteLine("Disconnecting from COM port");
+            try
+            {
+                if (IsOpen())
+                {
+                    serialPort.Close();
+                }
+            }
+            catch { }
+        }
+
+        public static void Write(string s)
+        {
+            try
+            {
+                if (IsOpen())
+                {
+                    serialPort.Write(s);
+                }
+            }
+            catch { }
+        }
+
+        public static void Write(byte[] bytes)
+        {
+            try
+            {
+                if (IsOpen())
+                {
+                    serialPort.Write(bytes, 0, bytes.Length);
+                    var str = "";
+                    for (var index = 0; index < 18; ++index)
+                    {
+                        str = str + bytes[index] + " ";
+                    }
+                    Console.WriteLine(str);
+                }
+                else
+                {
+                    Console.WriteLine("Serial port is not open");
+                }
+            }
+            catch { }
+        }
+
+        public static bool IsOpen()
+        {
+            try
+            {
+                return serialPort.IsOpen;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ src/Data transmitter on DOF/Data transmitter on DOF/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using Test_connected_to_COM_Port;
+
+var port = 12345;
+var ipAddress = IPAddress.Parse("127.0.0.1");
+var listener = new TcpListener(ipAddress, port);
+listener.Start();
+
+Console.WriteLine();
+
+Console.WriteLine(ComPort_SerialPortStream.TryConnect());
+
+var task = new Task(async () =>
+{
+    while (true)
+    {
+        var client = await listener.AcceptTcpClientAsync();
+        
+        await using (var stream = client.GetStream())
+        {
+            var receivedData = new byte[1024];
+            var bytesRead = await stream.ReadAsync(receivedData);
+            var actualData = new byte[bytesRead];
+            Array.Copy(receivedData, actualData, bytesRead);
+            ComPort_SerialPortStream.Write(actualData);
+            var receivedMessage = Encoding.UTF8.GetString(actualData);
+            Console.WriteLine("Полученные данные: " + receivedMessage);
+        }
+
+        client.Close();
+        Thread.Sleep(20);
+    }
+});
+
+task.Start();
+
+Console.ReadLine();
+
+task.Dispose();
